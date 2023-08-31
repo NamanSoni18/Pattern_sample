a014f081228cb6959b58da7cd445d5ccd43a23c0
Naman Soni <sknaman763@gmail.com>
2023-08-31 23:47:49 +0530

    updated files'

---
+++ solid_pattern.java
@@ -0,0 +1,14 @@
+public class solid_pattern {
+    public static void main(String[] args) {
+        
+        // Row
+        for(int i=1;i<=4;i++) {
+
+            // Column
+            for(int j = 1;j<=5;j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
